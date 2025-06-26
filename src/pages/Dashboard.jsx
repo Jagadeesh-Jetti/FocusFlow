24fe5c4bb0c091af73fa38b2140bf0e11f95d5b7
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-06-26 23:16:27 +0530

    refactor: pulled out side bar as a component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,48 +1,9 @@
+import { Sidebar } from '../components/Sidebar';
+
 export const Dashboard = () => {
   return (
     <div className="flex h-screen bg-gray-50">
-      <aside className="w-1/5 bg-gray-600 p-6 hidden md:block">
-        <h1 className="text-3xl font-serif font-bold text-white mb-8 m-1">
-          Goal Deck
-        </h1>
-        <ul className="space-y-4 text-white text-2xl font-serif">
-          <li>
-            <a href="/dashboard" className="hover:text-yellow-400">
-              🏠 Dashboard
-            </a>
-          </li>
-          <li>
-            <a href="/goals" className="hover:text-yellow-400">
-              🎯 Goals
-            </a>
-          </li>
-          <li>
-            <a href="/goals" className="hover:text-yellow-400">
-              🎯 Milestones
-            </a>
-          </li>
-          <li>
-            <a href="/goals" className="hover:text-yellow-400">
-              🎯 Tasks
-            </a>
-          </li>
-          <li>
-            <a href="/circles" className="hover:text-yellow-400">
-              👥 Circles
-            </a>
-          </li>
-          <li>
-            <a href="/progress" className="hover:text-yellow-400">
-              📈 Progress
-            </a>
-          </li>
-          <li>
-            <a href="/profile" className="hover:text-yellow-400">
-              ⚙️ Profile
-            </a>
-          </li>
-        </ul>
-      </aside>
+      <Sidebar />
       <div className="bg-blue-50 w-screen">
         <div className="text-5xl text-gray-800 font-medium p-5">
           Welcome Back! Jaggu
