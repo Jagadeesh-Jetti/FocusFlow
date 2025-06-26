4bda1e24a48bc4e94d0fb76a93521bd36eca61bc
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-06-26 23:45:36 +0530

    feat: added routes to the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { Dashboard } from './pages/Dashboard';
+import { Goals } from './pages/Goals';
 import { Login } from './pages/Login';
+import { Milestones } from './pages/Milestones';
 import { Signup } from './pages/SIgnup';
 import { Routes, Route } from 'react-router-dom';
 
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/goals" element={<Goals />} />
+        <Route path="/milestones" element={<Milestones />} />
       </Routes>
     </div>
   );
