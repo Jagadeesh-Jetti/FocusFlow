191ae6ed77d85bb8b3164290bc2fc281b9a7ee40
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-06-26 23:46:26 +0530

    chore: created basic milestones page

---
+++ src/pages/Milestones.jsx
@@ -0,0 +1,12 @@
+import { Sidebar } from '../components/Sidebar';
+
+export const Milestones = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="m-4">
+        <div className="text-4xl">Milestones</div>
+      </div>
+    </div>
+  );
+};
