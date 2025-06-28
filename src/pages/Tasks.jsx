3ade24ed9702c2b56ebb50047a591dba5474175a
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-06-28 15:26:28 +0530

    refactor: altered the UI layout of tasks page

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -4,23 +4,35 @@ export const Tasks = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="m-4">
+      <div className="m-4 w-screen">
         <div className="text-4xl m-3">Tasks</div>
-        <div>Filters </div>
+        <div className="flex justify-between m-4">
+          <div>Filters </div>
+          <div className="text-3xl font-semibold"> Add Task</div>
+        </div>
         <div>
-          <div className="text-2xl border m-2 p-2 w-80">
-            <input type="checkbox" className="" /> Complete tasks
+          <div className="flex gap-x-4 text-2xl border m-2 p-2 w-80">
+            <input type="checkbox" className="" />
+            <div>
+              <div>Complete tasks</div>
+              <div>Goal: Get Fit </div>
+            </div>
           </div>
-          <div className="text-2xl border m-2 p-2 w-80">
-            <input type="checkbox" className="" /> Complete backend
+          <div className="flex gap-x-4 text-2xl border m-2 p-2 w-80">
+            <input type="checkbox" className="" />
+            <div>
+              <div>Complete backend</div>
+              <div>Goal: get a job </div>
+            </div>
           </div>
-          <div className="text-2xl border m-2 p-2 w-80">
-            <input type="checkbox" className="" /> Code backend
+          <div className="flex gap-x-4 text-2xl border m-2 p-2 w-80">
+            <input type="checkbox" className="" />
+            <div>
+              <div>Code backend</div>
+              <div>Goal: Get fit</div>
+            </div>
           </div>
         </div>
-        <button className="bg-gray-600 text-white font-semibold w-40 rounded-md p-3 m-2">
-          Add Task
-        </button>
       </div>
     </div>
   );
