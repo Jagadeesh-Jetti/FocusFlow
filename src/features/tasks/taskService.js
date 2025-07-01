5792d1959baa9d7e5a18ae51cc07ceb07ac7f883
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-07-01 23:50:22 +0530

    refactor; updated api response points

--- src/features/tasks/taskService.js
+++ src/features/tasks/taskService.js
@@ -2,24 +2,24 @@ import axios from '../../utils/api';
 
 export const getTasksAPI = async () => {
   const response = await axios.get('/tasks');
-  return response.data;
+  return response.data.tasks;
 };
 export const createTaskAPI = async (userData) => {
   const response = await axios.post('/tasks', userData);
-  return response.data;
+  return response.data.task;
 };
 
 export const getTaskByIdAPI = async (id) => {
   const response = await axios.get(`/tasks/${id}`);
-  return response.data;
+  return response.data.task;
 };
 
 export const updateTaskByIdAPI = async (id, updatedData) => {
-  const response = await axios.put('/tasks/${id}', updatedData);
-  return response.data;
+  const response = await axios.put(`/tasks/${id}`, updatedData);
+  return response.data.task;
 };
 
 export const deleteTaskByIdAPI = async (id) => {
   const response = await axios.delete(`/tasks/${id}`);
-  return response.data;
+  return response.data.task;
 };
