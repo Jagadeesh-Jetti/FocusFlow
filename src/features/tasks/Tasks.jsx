eeb991744b6c08f5f6895bd38b2e7eb8f48701f3
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-07-02 23:46:24 +0530

    feat: added edit and delete features

--- src/features/tasks/Tasks.jsx
+++ src/features/tasks/Tasks.jsx
@@ -2,7 +2,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Modal } from '../../components/Modal';
 import { Sidebar } from '../../components/Sidebar';
 import { useEffect, useState } from 'react';
-import { getTasks } from './taskThunk';
+import {
+  createTask,
+  deleteTaskById,
+  getTasks,
+  updateTaskById,
+} from './taskThunk';
+import { getMilestones } from '../milestones/milestoneThunk';
 
 export const Tasks = () => {
   const milestones = useSelector((state) => state.milestone.milestoneList);
@@ -10,6 +16,8 @@ export const Tasks = () => {
   console.log(milestones);
   const dispatch = useDispatch();
   const [showModal, setShowModal] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
   const [form, setForm] = useState({
     title: '',
     description: '',
@@ -19,14 +27,58 @@ export const Tasks = () => {
     isCompleted: false,
   });
 
-  useEffect(() => {
-    dispatch(getTasks());
-  }, [dispatch]);
+  const resetForm = () => {
+    setForm({
+      title: '',
+      description: '',
+      milestone: '',
+      priority: 'medium',
+      dueDate: '',
+      isCompleted: false,
+    });
+    setEditId(null);
+    setIsEditing(false);
+    setShowModal(false);
+  };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     console.log(form);
     e.preventDefault();
+
+    if (isEditing) {
+      await dispatch(updateTaskById({ id: editId, updatedData: form }));
+    } else {
+      await dispatch(createTask(form));
+    }
+
+    await dispatch(getTasks());
+
+    resetForm();
   };
+
+  const editHandler = async (id, task) => {
+    setShowModal(true);
+    setIsEditing(true);
+    setEditId(id);
+    setForm({
+      title: task.title,
+      description: task.description,
+      milestone: task.milestone,
+      priority: task.priority,
+      dueDate: task.dueDate,
+      isCompleted: task.isCompleted,
+    });
+  };
+
+  const deleteHandler = async (id) => {
+    await dispatch(deleteTaskById(id));
+    await dispatch(getTasks());
+  };
+
+  useEffect(() => {
+    dispatch(getTasks());
+    dispatch(getMilestones());
+  }, [dispatch]);
 
   return (
     <div className="flex">
@@ -43,13 +95,14 @@ export const Tasks = () => {
         </div>
         <Modal
           isOpen={showModal}
-          onClose={() => setShowModal(false)}
+          onClose={() => resetForm()}
           title="Add New Task"
         >
           <form onSubmit={handleSubmit} className="space-y-4 p-4">
             <input
               type="text"
               name="title"
+              value={form.title}
               placeholder="Task Title"
               className="w-full p-2 border rounded"
               onChange={(e) => setForm({ ...form, title: e.target.value })}
@@ -60,6 +113,7 @@ export const Tasks = () => {
               name="description"
               placeholder="Task Description"
               rows={4}
+              value={form.description}
               className="w-full p-2 border rounded"
               onChange={(e) =>
                 setForm({ ...form, description: e.target.value })
@@ -71,21 +125,21 @@ export const Tasks = () => {
               name="milestone"
               className="w-full p-2 border rounded"
               required
+              value={form.milestone}
               onChange={(e) => setForm({ ...form, milestone: e.target.value })}
             >
               <option value="">Select Milestone</option>
-              {milestones.length === 0
-                ? 'milestone'
-                : milestones.map((milestone) => (
-                    <option key={milestone._id} value={milestone._id}>
-                      {milestone.title}
-                    </option>
-                  ))}
+              {milestones.map((milestone) => (
+                <option key={milestone._id} value={milestone._id}>
+                  {milestone.title}
+                </option>
+              ))}
             </select>
 
             <select
               name="priority"
               className="w-full p-2 border rounded"
+              value={form.priority}
               onChange={(e) => setForm({ ...form, priority: e.target.value })}
               required
             >
@@ -97,8 +151,9 @@ export const Tasks = () => {
             <input
               type="date"
               name="dueDate"
+              value={form.dueDate?.split('T')[0] || ''}
               className="w-full p-2 border rounded"
-              onChange={(e) => setForm({ ...form, dueData: e.target.value })}
+              onChange={(e) => setForm({ ...form, dueDate: e.target.value })}
               required
             />
 
@@ -106,10 +161,11 @@ export const Tasks = () => {
               <input
                 type="checkbox"
                 name="isCompleted"
+                checked={form.isCompleted}
                 id="isCompleted"
                 className="w-5 h-5 accent-green-600"
                 onChange={(e) =>
-                  setForm({ ...form, isCompleted: e.target.value })
+                  setForm({ ...form, isCompleted: e.target.checked })
                 }
               />
               <label htmlFor="isCompleted" className="text-sm text-gray-700">
@@ -137,10 +193,16 @@ export const Tasks = () => {
                 <div> {task.title} </div>
               </div>
               <div className="flex">
-                <div className="border bg-gray-200 font-black p-1 m-2 rounded-md cursor-pointer hover:scale-110">
+                <div
+                  className="border bg-gray-200 font-black p-1 m-2 rounded-md cursor-pointer hover:scale-110"
+                  onClick={() => editHandler(task._id, task)}
+                >
                   Edit
                 </div>
-                <div className="border bg-gray-200  font-black p-1 m-2 rounded-md cursor-pointer hover:scale-110">
+                <div
+                  className="border bg-gray-200  font-black p-1 m-2 rounded-md cursor-pointer hover:scale-110"
+                  onClick={() => deleteHandler(task._id)}
+                >
                   Delete
                 </div>
               </div>
