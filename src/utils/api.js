62a63cb76bb9060d6e11a296ec53dda257dc7b0f
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-07-03 23:49:06 +0530

    feat: added  api interceptors

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,4 +7,29 @@ const axiosInstance = axios.create({
   },
 });
 
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error?.resposne?.status;
+    const message = error?.response?.data?.message;
+
+    if (
+      status === 401 &&
+      (message === 'Token invalid or expired' ||
+        message === 'Not authorized, token missing')
+    ) {
+      console.warn('🔐 Token expired or unauthorized. Logging out...');
+      localStorage.clear();
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
