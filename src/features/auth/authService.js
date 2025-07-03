b4b3fa5027e6255cc149cc1b5a22c47fb5d610d7
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-07-03 23:48:31 +0530

    chore: cleaned up consoles

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -2,12 +2,11 @@ import axios from '../../utils/api';
 
 export const loginUserAPI = async (userData) => {
   const response = await axios.post('/auth/login', userData);
-  console.log(response);
   return response.data;
 };
 
 export const registerUserAPI = async (userData) => {
   const response = await axios.post('/auth/register', userData);
-  console.log(response);
+
   return response.data;
 };
