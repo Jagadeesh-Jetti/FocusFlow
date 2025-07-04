6f68d6719d3eae8292d4aa6922598fb13aa1569e
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-07-04 23:51:10 +0530

    feat: created feed thunk

---
+++ src/features/community/feedThunk.js
@@ -0,0 +1,83 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import {
+  commentPostAPI,
+  createPostAPI,
+  deletePostAPI,
+  getPostByIdAPI,
+  getPostsAPI,
+} from './feedService';
+
+export const getPosts = createAsyncThunk('post/getAll', async (_, thunkAPI) => {
+  try {
+    return await getPostsAPI();
+  } catch (err) {
+    return thunkAPI.rejectWithValue(
+      err.response.data.message || 'Posts retrieval failed'
+    );
+  }
+});
+
+export const createPost = createAsyncThunk(
+  'post/create',
+  async (postData, thunkAPI) => {
+    try {
+      return await createPostAPI(postData);
+    } catch (err) {
+      return thunkAPI.rejectWithValue(
+        err.response.data.message || 'Post creation failed'
+      );
+    }
+  }
+);
+
+export const getPostById = createAsyncThunk(
+  'post/getById',
+  async (id, thunkAPI) => {
+    try {
+      return await getPostByIdAPI(id);
+    } catch (err) {
+      return thunkAPI.rejectWithValue(
+        err.response.data.message || 'Post retrieval by id is failed'
+      );
+    }
+  }
+);
+
+export const toggleLikePost = createAsyncThunk(
+  'post/toggleLike',
+  async (id, thunkAPI) => {
+    try {
+      return await toggleLikePost(id);
+    } catch (err) {
+      return thunkAPI.rejectWithValue(
+        err.response.data.message || 'Toggle like is failed'
+      );
+    }
+  }
+);
+
+export const commentPost = createAsyncThunk(
+  'post/comment',
+  async ({ id, comment }, thunkAPI) => {
+    try {
+      return await commentPostAPI(id, comment);
+    } catch (err) {
+      return thunkAPI.rejectWithValue(
+        err.response.data.message || 'Commenting post failed'
+      );
+    }
+  }
+);
+
+export const deletePost = createAsyncThunk(
+  'post/delete',
+  async (id, thunkAPI) => {
+    try {
+      return await deletePostAPI(id);
+    } catch (err) {
+      return thunkAPI.rejectWithValue(
+        err.response.data.message || 'Deleting post failed'
+      );
+    }
+  }
+);
