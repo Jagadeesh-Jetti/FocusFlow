2b3fc564d8cb6cf37752e282b5d564200f975e53
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-07-31 16:59:20 +0530

    feat: created weekly chart component

---
+++ src/features/Dashboard/dashboardComponents/WeeklyBarChart.jsx
@@ -0,0 +1,38 @@
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
+
+export const WeeklyBarChart = ({ data }) => {
+  const chartData = {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    datasets: [
+      {
+        label: 'Tasks Done',
+        data,
+        backgroundColor: '#3b82f6',
+        borderRadius: 6,
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: { display: false },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <Bar data={chartData} options={options} />;
+};
